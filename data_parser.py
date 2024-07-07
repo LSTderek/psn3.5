de296622432dab97f456bd48f67d9147fcc6deba
LSTderek <derek@lucentstage.com>
2024-07-06 21:32:53 -0400

    Update data_parser.py

--- data_parser.py
+++ data_parser.py
@@ -1,46 +1,30 @@
 import logging
 from multiprocessing.connection import Listener
-import pickle
 
 # Configuration for logging
-LOG_TO_FILE = False
-LOG_TO_CONSOLE = True
-LOG_FILE = 'data_parser.log'
-
-# Set up logging
 logger = logging.getLogger('DataParser')
 logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-if LOG_TO_FILE:
-    file_handler = logging.FileHandler(LOG_FILE)
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
-
-if LOG_TO_CONSOLE:
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(formatter)
-    logger.addHandler(console_handler)
-
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
 
 def start_data_parser():
     address = ('localhost', 6000)
-    listener = Listener(address, authkey=b'data_parser')
+    listener = Listener(address, authkey=b'secret')
     logger.info("DataParser started and waiting for connections...")
-
     while True:
         with listener.accept() as conn:
-            logger.info('Connection accepted from ' + str(listener.last_accepted))
-            while True:
-                try:
+            logger.info(f"Connection accepted from {listener.last_accepted}")
+            try:
+                while True:
                     data = conn.recv()
-                    if data:
-                        chunk_data = pickle.loads(data)
-                        logger.info(f"Received PSN_DATA_PACKET: {chunk_data}")
-                except EOFError:
-                    logger.info("Connection closed")
-                    break
-
+                    if not data:
+                        break
+                    logger.info(f"Received PSN_DATA_PACKET: {data}")
+            except EOFError:
+                logger.info("Connection closed")
 
 if __name__ == "__main__":
     start_data_parser()
